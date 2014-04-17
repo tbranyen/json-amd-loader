c4890cbb80e371c3668804848b0175a38779aad2
Tim Branyen <tim@tabdeveloper.com>
2014-04-17 12:03:22 -0400

    Initial commit.

---
+++ loader.js
@@ -0,0 +1,36 @@
+define(function(require, exports) {
+  "use strict";
+
+  // Cache used to map configuration options between load and write.
+  var buildMap = {};
+
+  exports.load = function(name, req, load, config) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (config.isBuild) {
+          return load();
+        }
+
+        try {
+          load(buildMap[name] = JSON.parse(xhr.responseText));
+        }
+        catch (ex) {
+          load.error(ex);
+        }
+      }
+    };
+
+    // Pass errors through.
+    xhr.onerror = load.error;
+
+    xhr.open("GET", req.toUrl(name + ".json"), true);
+    xhr.send(null);
+  };
+
+  exports.write = function(pluginName, moduleName, write) {
+    var stringified = JSON.stringify(buildMap[moduleName]);
+    write(["define('", pluginName, "!", moduleName, "', ", stringified, ")"]);
+  };
+});
